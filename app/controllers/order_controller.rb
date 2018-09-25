6821553e70828cd096f2ab9f03c9687681e29d9d
amine1231996 <amine1231996@outlook.com>
2018-09-25 17:23:06 -0400

    Test

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,5 @@
 class OrderController < ApplicationController
-     skip_before_action :verify_authenticity_token, :only => [:show, :create]
+     skip_before_action :verify_authenticity_token, :only => [:index, :show, :create]
   
    # GET /pizzas
   # GET /pizzas.json
