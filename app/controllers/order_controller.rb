42ffb8434a69ece3ac40ed8b420e5e3e8e09e1ce
amine1231996 <amine1231996@outlook.com>
2018-09-27 13:16:47 -0400

    test msg

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -9,4 +9,30 @@ class OrderController < ApplicationController
        # GET /pizzas.json
        def index
        end
+       def create 
+          p "ordersController"
+          @nom = params[:name]
+          @adresse = params[:adress]
+          p @adresse
+          @pizzas = params[:pizzas]
+          newOrder = Order.new(:name => @nom, :adress => @adresse)
+          newOrder.save
+          p @pizzas
+          @erros="Erros : [";
+          @pizzas.each do |pizza|
+        @orderPizza =Pizza.where(name: pizza).first
+        if @orderPizza.nil?
+          @erros= @erros+" - There is no pizza with name = "+pizza
+          p @erros
+          next
+        end 
+        p @orderPizza
+        newOrderline = Orderline.new
+        newOrderline.order=newOrder
+        newOrderline.pizza=@orderPizza
+        newOrderline.save
+          end
+          @erros = @erros + "]"
+      end
+// -- //        
 end
