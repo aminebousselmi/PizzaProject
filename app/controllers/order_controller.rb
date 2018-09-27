314b05734730c60b86c88a2fbbdd1cd3b740440d
amine1231996 <amine1231996@outlook.com>
2018-09-27 15:56:48 -0400

    error

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -26,5 +26,21 @@ def create
     @erros = @erros + "]"
 end
   
+   def view
+    @orders= []
+    @allOrders = Order.all
+    @allOrders.each do |order|
+      pizzas = []
+      @orderlines=Orderline.where(order: order)
+      @orderlines.each do |orderline|
+          pizza = Pizza.find(orderline[:pizza_id])
+          pizzas.push(pizza)
+      end
+      p "orders "
+      @orders.push({order: order ,pizzas: pizzas})
+      p pizzas
+    end
+end
+  
   
 end
