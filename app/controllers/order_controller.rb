5e9f3d0dc5d6eceabda209f66e9a423b59a70d50
amine1231996 <amine1231996@outlook.com>
2018-09-27 13:42:50 -0400

    test msg

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -9,16 +9,7 @@ class OrderController < ApplicationController
        def index
        end
   
-       def create
-      @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
+     
   end
 // -- //        
 end
