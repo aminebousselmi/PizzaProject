aab3ca9b0942eda1b9061d63315ebf1c256800e9
amine1231996 <amine1231996@outlook.com>
2018-09-27 14:43:56 -0400

    rg error

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,28 @@
-class OrderController < ApplicationController      
+class OrderController < ApplicationController    
+      skip_before_action :verify_authenticity_token, :only => [:create]
+def create 
+    p "orderController"
+    @name = params[:name]
+    @adress = params[:adress]
+    p @adress
+    @pizzas = params[:pizzas]
+    newOrder = Order.new(:name => @name, :adress => @adress)
+    newOrder.save
+    p @pizzas
+    @erros="Erros : [";
+    @pizzas.each do |pizza|
+	@orderPizza =Pizza.where(name: pizza).first
+	if @orderPizza.nil?
+	  @erros= @erros+" - There is no pizza with name = "+pizza
+	  p @erros
+	  next
+	end 
+	p @orderPizza
+	newOrderline = Orderline.new
+	newOrderline.order=newOrder
+	newOrderline.pizza=@orderPizza
+	newOrderline.save
+    end
+    @erros = @erros + "]"
+end
 end
