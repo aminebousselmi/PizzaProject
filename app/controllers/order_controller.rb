dcb099d148045b509033889fbb787b7fbeff73ad
amine1231996 <amine1231996@outlook.com>
2018-09-27 14:17:51 -0400

    rg error

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,15 +1,2 @@
-class OrderController < ApplicationController
-     skip_before_action :verify_authenticity_token, :only => [:index, :show]
-      # GET /order
-      # GET /order.json
-      def show
-      end
-  
-       
-       def index
-       end
-  
-     
-  end
-// -- //        
+class OrderController < ApplicationController      
 end
