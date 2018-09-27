a83cf641e283b7bf1b0cf268c63939df781e1a60
amine1231996 <amine1231996@outlook.com>
2018-09-27 13:34:46 -0400

    test msg

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,34 +5,20 @@ class OrderController < ApplicationController
       def show
       end
   
-       # GET /pizzas
-       # GET /pizzas.json
+       
        def index
        end
-       def create 
-          p "ordersController"
-          @nom = params[:name]
-          @adresse = params[:adress]
-          p @adresse
-          @pizzas = params[:pizzas]
-          newOrder = Order.new(:name => @nom, :adress => @adresse)
-          newOrder.save
-          p @pizzas
-          @erros="Erros : [";
-          @pizzas.each do |pizza|
-        @orderPizza =Pizza.where(name: pizza).first
-        if @orderPizza.nil?
-          @erros= @erros+" - There is no pizza with name = "+pizza
-          p @erros
-          next
-        end 
-        p @orderPizza
-        newOrderline = Orderline.new
-        newOrderline.order=newOrder
-        newOrderline.pizza=@orderPizza
-        newOrderline.save
-          end
-          @erros = @erros + "]"
+  
+       def create
+      @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
+    end
+  end
 // -- //        
 end
