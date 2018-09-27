4f939af331224f0f3c62606bc3e9f6d203688b4a
amine1231996 <amine1231996@outlook.com>
2018-09-27 14:50:04 -0400

    rg error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   post 'order/create', to: 'order#create'
-  resources :pizzasS
+  resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
