b882e2eceb38fe7e2bd16f4ac1504ff7189d6157
amine1231996 <amine1231996@outlook.com>
2018-10-02 03:48:49 -0400

    test get

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   post 'order/create', to: 'order#create'
   get 'order/show', to: 'order#view'
-   get 'order/view', to: 'order#index'
   resources :pizzas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
